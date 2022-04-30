ffd1b90053d44c50aba737f2b508cb10394ac77a
GLAZUNOV ALEKSEI <993468@gmail.com>
2022-04-30 20:14:14 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
-import About from "./pages/About/About";
+import About from "./pages/About";
 import Biography from "./pages/Biography";
 import Characters from "./pages/Characters";
 import Contacts from "./pages/Contacts";
-import Main from "./pages/Main/Main";
+import Main from "./pages/Main";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Main />} />
-        {/* <Route path="bio/:id" element={<Biography />} /> */}
         <Route path="characters" element={<Characters />} />
         <Route path="characters/:id" element={<Biography />} />
         <Route path="about" element={<About />} />
--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+
   document.getElementById("root")
 );
