8f92249d27e80d821abff080c5c934268c4061d7
GLAZUNOV ALEKSEI <993468@gmail.com>
2022-06-16 18:55:18 +0400

    homework-8

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { createContext, useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import About from "./pages/About";
@@ -6,19 +7,42 @@ import Characters from "./pages/Characters";
 import Contacts from "./pages/Contacts";
 import Login from "./pages/Login";
 import Main from "./pages/Main";
+import { CHARACTER } from "./pages/Main/characters";
+
+export const Context = createContext(null);
 
 function App() {
+  const [character, setCharacter] = useState(() => {
+    return JSON.parse(localStorage.getItem("character")) || CHARACTER;
+  });
+
+  const handleLikeClick = (id) => {
+    setCharacter((prev) =>
+      prev.map((item) => {
+        if (item.id === id) {
+          return { ...item, isLike: !item.isLike };
+        }
+        return item;
+      })
+    );
+  };
+  useEffect(() => {
+    localStorage.setItem("character", JSON.stringify(character));
+  }, [character]);
+
   return (
-    <Routes>
-      <Route path="/login" element={<Login />}></Route>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Main />} />
-        <Route path="characters" element={<Characters />} />
-        <Route path="characters/:id" element={<Biography />} />
-        <Route path="about" element={<About />} />
-        <Route path="contacts" element={<Contacts />} />
-      </Route>
-    </Routes>
+    <Context.Provider value={{ character, handleLikeClick }}>
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Main />} />
+          <Route path="characters" element={<Characters />} />
+          <Route path="characters/:id" element={<Biography />} />
+          <Route path="about" element={<About />} />
+          <Route path="contacts" element={<Contacts />} />
+        </Route>
+      </Routes>
+    </Context.Provider>
   );
 }
 
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,24 +1,25 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
+import { Context } from "../../App";
 import CharacterCard from "../../components/CharacterCard";
 import Container from "../../components/Container";
 import Heading from "../../components/Heading";
 import Slider from "../../components/Slider";
-import { CHARACTER } from "./characters";
+
 import s from "./Main.module.scss";
 
 export default function Main() {
-  const [character, setCharacter] = useState(CHARACTER);
+  const { character, handleLikeClick } = useContext(Context);
 
-  const handleLikeClick = (id) => {
-    setCharacter((prev) =>
-      prev.map((item) => {
-        if (item.id === id) {
-          return { ...item, isLike: !item.isLike };
-        }
-        return item;
-      })
-    );
-  };
+  // const handleLikeClick = (id) => {
+  //   setCharacter((prev) =>
+  //     prev.map((item) => {
+  //       if (item.id === id) {
+  //         return { ...item, isLike: !item.isLike };
+  //       }
+  //       return item;
+  //     })
+  //   );
+  // };
 
   return (
     <>
@@ -44,7 +45,7 @@ export default function Main() {
                     humanName={item.humanName}
                     id={item.id}
                     isLike={item.isLike}
-                    onLikeClick={handleLikeClick}
+                    onLikeClick={() => handleLikeClick(item.id)}
                   />
                 </div>
               );
